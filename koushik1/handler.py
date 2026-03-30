a323632266b8f4b22265c02de771ade982a264b8
nkoushik30 <koushiknelluri@KOUSHIK.localdomain>
2026-03-30 11:12:30 +0530

    handler file is modified

--- koushik1/handler.py
+++ koushik1/handler.py
@@ -1,12 +1,2 @@
-import requests
-
-url = "http://localhost:8080/function/koushik1"
-
-data = {
-    "a": 10,
-    "b": 20
-}
-
-response = requests.post(url, json=data)
-
-print(response.json())
+def handle(event, context):
+    return ("15", 200)
